a32970c9f06fbee6c824c000989cfea6e010db1e
sewinx07 <taha.gmir@esprit.tn>
2026-07-03 16:31:50 +0100

    Make Prisma client lazy to avoid build-time DATABASE_URL crash

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -13,6 +13,15 @@ function makeClient() {
   return new PrismaClient({ adapter });
 }
 
-export const prisma = globalForPrisma.prisma ?? makeClient();
+function getPrisma(): PrismaClient {
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = makeClient();
+  }
+  return globalForPrisma.prisma;
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = new Proxy({} as PrismaClient, {
+  get(_, prop) {
+    return getPrisma()[prop as keyof PrismaClient];
+  },
+});
